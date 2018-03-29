745a4e9db4979d2844edc7e7f6625c8a26593a28
ANDREY STERKHOV <bess410@hotmail.com>
2018-03-29 22:50:27 +0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,9 +4,9 @@ $(document).ready(function(){
 		var pass = $("#pass").val();
 		
 		if(login == "user" && pass == "user"){
-			$("#login_form").attr("action", "user_requests.html");
+			$("#login_form").attr("action", "/user_requests.html");
 		} else if(login == "admin" && pass == "admin"){
-			$("#login_form").attr("action", "admin_requests.html");
+			$("#login_form").attr("action", "/admin_requests.html");
 		} else{
 			$("#login").val(login);
 			$("#pass").val(pass);
